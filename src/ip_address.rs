5f348c4b4928f4f937c0ccaea9c79ff5bbf2ab6a
Josh Madden <cyrex562@gmail.com>
2022-03-03 15:23:36 -0500

    Update ip_address.rs

--- src/ip_address.rs
+++ src/ip_address.rs
@@ -3,7 +3,19 @@ pub enum IpAddressType {
     IPv6,
 }
 
+#[derive(Clone,Debug,Default)]
 pub struct IpAddress {
     address_bytes: Vec<u8>,
     address_type: IpAddressType,
+}
+
+impl IpAddress {
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    pub fn  init_ipv4(&mut self, addr: u32) {
+        let mut u32_bytes: [u8;4] = [0,0,0,0];
+        u32_bytes = addr::from()
+    }
 }
